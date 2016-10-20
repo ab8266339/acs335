9cbcc07ded3ef244ae6f2f549753b632594ee0d5
Hao <Hxu9@sheffield.ac.uk>
2016-10-20 20:12:15 +0100

    update for ex2

--- Lab2/main.c
+++ Lab2/main.c
@@ -4,6 +4,7 @@
 
 
 int main(void){
+	unsigned ii;
 	RCC->AHB1ENR |=RCC_AHB1ENR_GPIODEN;
 	GPIOD->MODER |= GPIO_MODER_MODER12_0;// GPIOD pin 12 output-green LED
 	//Initialize Timer 2
@@ -12,19 +13,20 @@ int main(void){
 	TIM2->CR1  = 1<<3;
 	TIM2->PSC = 8400-1;
 	TIM2->ARR =1905-1;
-	TIM2->EGR = 1<<0;
+	TIM2->EGR = 1;
 	
-	
-
-
-
+for(;;){
 	GPIOD->BSRR = 1<<12;
-	TIM2->CR1 |= TIM_CR1_CEN;
+	//TIM2->CR1 |= TIM_CR1_CEN;
+	TIM2->CR1 = 1<<0;
+	//for(ii=0;ii<2600000;ii++){}
 	while((TIM2->SR&0x0001)!=1){}; 
 	TIM2->SR  &= ~TIM_SR_UIF;
 	GPIOD->BSRR = 1<<(12+16);
 	TIM2->CR1 |= TIM_CR1_CEN;
+	//for(ii=0;ii<2600000;ii++){}
 	while((TIM2->SR&0x0001)!=1){}; 
 	TIM2->SR  &= ~TIM_SR_UIF;
 	
+}
 }
